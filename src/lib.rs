a67b472da7bc2e96a9a5892b954a70ac398bf054
Jakob Hellermann <jakob.hellermann@protonmail.com>
2019-07-24 11:44:54 +0200

    fix typo

--- src/lib.rs
+++ src/lib.rs
@@ -8,7 +8,7 @@
     missing_debug_implementations
 )]
 
-//! An asynchronous library for discovering, notifying and subscibing to devices and services on a network.
+//! An asynchronous library for discovering, notifying and subscribing to devices and services on a network.
 //!
 //! SSDP stands for Simple Service Discovery Protocol and it is a protocol that
 //! distributes messages across a local network for devices and services to
