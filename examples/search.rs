4a259a1c4b92fae2b1c9968624ff1be015a212cd
Jakob Hellermann <jakob.hellermann@protonmail.com>
2019-08-29 16:12:45 +0200

    better example

--- examples/search.rs
+++ examples/search.rs
@@ -6,13 +6,18 @@ use std::time::Duration;
 
 #[runtime::main]
 async fn main() -> Result<(), ssdp_client::Error> {
-    let search_target = SearchTarget::RootDevice;
+    //let search_target = SearchTarget::RootDevice;
+    let search_target: SearchTarget = "urn:schemas-upnp-org:device:ZonePlayer:1".parse().unwrap();
     let timeout = Duration::from_secs(3);
     let stream = ssdp_client::search(search_target, timeout, 2).await?;
 
     #[for_await]
     for response in stream {
-        println!("{:?}", response);
+        let response = response?;
+        println!("- {}", response.search_target());
+        println!("  - location: {}", response.location());
+        println!("  - usn: {}", response.usn());
+        println!("  - server: {}", response.server());
     }
 
     Ok(())
